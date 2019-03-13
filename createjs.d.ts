97ba2c5a06e7395bb6af517cf04d4f8fce8d0c0e
Denis Murphy <dmurphy@dairymaster.com>
2019-03-13 17:58:15 +0000

    code changes

--- createjs.d.ts
+++ createjs.d.ts
@@ -183,8 +183,8 @@ declare namespace createjs {
     }
 
     export class ButtonHelper<T extends DisplayObject> {
-        constructor(target: Sprite, outLabel?: string|number, overLabel?: string|number, downLabel?: string|number, play?: boolean, hitArea?: T, hitLabel?: string);
-        constructor(target: MovieClip, outLabel?: string|number, overLabel?: string|number, downLabel?: string|number, play?: boolean, hitArea?: T, hitLabel?: string);
+        constructor(target: Sprite, outLabel?: string|number, overLabel?: string|number, downLabel?: string|number, play?: boolean, hitArea?: T, hitLabel?: string|number);
+        constructor(target: MovieClip, outLabel?: string|number, overLabel?: string|number, downLabel?: string|number, play?: boolean, hitArea?: T, hitLabel?: string|number);
 
         // properties
         downLabel: string | number;
